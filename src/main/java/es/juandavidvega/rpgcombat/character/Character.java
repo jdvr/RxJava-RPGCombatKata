38adb188dcf78829d712ca799147059534bde7de
Juan David Vega <jdvr.93@hotmail.com>
2016-05-15 20:07:23 +0200

    unsubscribe all events when character is dead

--- src/main/java/es/juandavidvega/rpgcombat/character/Character.java
+++ src/main/java/es/juandavidvega/rpgcombat/character/Character.java
@@ -2,12 +2,16 @@ package es.juandavidvega.rpgcombat.character;
 
 import es.juandavidvega.rpgcombat.engine.EventBus;
 import es.juandavidvega.rpgcombat.engine.events.DamageEvent;
+import es.juandavidvega.rpgcombat.engine.events.EventTypes;
 import es.juandavidvega.rpgcombat.engine.events.GameEventChecker;
 import es.juandavidvega.rpgcombat.engine.events.IncreaseLifeEvent;
+import es.juandavidvega.rpgcombat.engine.subscription.Subscriptions;
+import rx.Subscription;
 
 public class Character {
 
     private Health health;
+    private final Subscriptions subscriptions = new Subscriptions();
 
     public Character(Health health) {
         this.health = health;
@@ -16,17 +20,19 @@ public class Character {
     }
 
     private void listenHealth() {
-        EventBus.get().toObserverable()
+        Subscription subscription = EventBus.get().toObserverable()
                 .filter(event -> new GameEventChecker().isLifeIncrease(event))
                 .map(gameEvent -> (IncreaseLifeEvent) gameEvent)
                 .subscribe(this::manageHealth);
+        subscriptions.add(EventTypes.IncreaseLife, subscription);
     }
 
     private void listenDamages() {
-        EventBus.get().toObserverable()
+        Subscription subscribe = EventBus.get().toObserverable()
                 .filter(event -> new GameEventChecker().isDamage(event))
                 .map(gameEvent -> (DamageEvent) gameEvent)
                 .subscribe(this::manageDamage);
+        subscriptions.add(EventTypes.Damage, subscribe);
     }
 
     private void manageDamage(DamageEvent damageEvent) {
@@ -39,6 +45,7 @@ public class Character {
 
     public void receive(Integer damage) {
         health.subtract(damage);
+        if(AmIDead()) subscriptions.unsubscribeAll();
     }
 
     public boolean isAlive() {
@@ -51,5 +58,9 @@ public class Character {
 
     public void health(Integer health) {
         this.health.add(health);
+    }
+
+    private boolean AmIDead() {
+        return !isAlive();
     }
 }
--- src/main/java/es/juandavidvega/rpgcombat/engine/subscription/Subscriptions.java
+++ src/main/java/es/juandavidvega/rpgcombat/engine/subscription/Subscriptions.java
@@ -1,4 +1,30 @@
 package es.juandavidvega.rpgcombat.engine.subscription;
 
+import es.juandavidvega.rpgcombat.engine.Event;
+import es.juandavidvega.rpgcombat.engine.events.EventTypes;
+import rx.Subscription;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Subscriptions {
+    private final Map<EventTypes, Subscription> subscriptions = new HashMap<>();
+
+    public void add(EventTypes event, Subscription subscription){
+        subscriptions.put(event, subscription);
+    }
+
+    public void unsubscribe(EventTypes event){
+        Subscription subscription = subscriptions.get(event);
+        if (subscription == null) return;
+        subscription.unsubscribe();
+        subscriptions.remove(event);
+    }
+
+    public void unsubscribeAll() {
+        subscriptions
+                .values()
+                .forEach(subscription -> subscription.unsubscribe());
+        subscriptions.clear();
+    }
 }
