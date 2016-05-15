d7369d0188df3fc6ba46a0e772b994fde46218ee
Juan David Vega <jdvr.93@hotmail.com>
2016-05-15 13:39:55 +0200

    red: drop_health_to_zero_when_damage_is_higher_than_health

--- src/main/java/es/juandavidvega/rpgcombat/character/RPGCharacter.java
+++ src/main/java/es/juandavidvega/rpgcombat/character/RPGCharacter.java
@@ -1,4 +1,23 @@
 package es.juandavidvega.rpgcombat.character;
 
 public class RPGCharacter {
+
+    private Integer health;
+
+    public RPGCharacter(Integer health) {
+
+        this.health = health;
+    }
+
+    public void receive(Integer damage) {
+
+    }
+
+    public boolean isAlive() {
+        return false;
+    }
+
+    public Integer health() {
+        return null;
+    }
 }
--- src/test/java/es/juandavidvega/rpgcombat/CharacterShould.java
+++ src/test/java/es/juandavidvega/rpgcombat/CharacterShould.java
@@ -1,7 +1,25 @@
 package es.juandavidvega.rpgcombat;
 
-/**
- * Created by jdvr on 15/05/16.
- */
+import es.juandavidvega.rpgcombat.character.RPGCharacter;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CharacterShould {
+
+    private static final Integer START_HEALTH = 1000;
+    private static final Integer MIN_HEALTH = 1000;
+    private static final Integer HIGHEST_DAMAGE = 1001;
+
+    @Test
+    public void
+    drop_health_to_zero_when_damage_is_higher_than_health() {
+        RPGCharacter target = new RPGCharacter(START_HEALTH);
+        target.receive(HIGHEST_DAMAGE);
+        assertThat(target.isAlive()).isTrue();
+        assertThat(target.health()).isEqualTo(MIN_HEALTH);
+    }
+
+
 }
