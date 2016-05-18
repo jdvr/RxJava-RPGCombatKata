697ef7e01b5ecbe5cf41177e58ae69dfdbb46553
Juan David Vega <jdvr.93@hotmail.com>
2016-05-18 20:44:44 +0200

    change EventType distributation

--- src/main/java/es/juandavidvega/rpgcombat/engine/events/EventType.java
+++ src/main/java/es/juandavidvega/rpgcombat/engine/events/EventType.java
@@ -1,5 +1,12 @@
 package es.juandavidvega.rpgcombat.engine.events;
 
 public enum EventType {
-    Damage, Health, IncreaseLife, JoinFaction, LeaveFaction, AttackProps, DamageProps, Attack
+    Damage,
+    Health,
+    IncreaseLife,
+    JoinFaction,
+    LeaveFaction,
+    AttackProps,
+    DamageProps,
+    Attack
 }
